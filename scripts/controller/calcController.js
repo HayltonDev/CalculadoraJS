c299237487f566124810a77678310cf35140e8e8
Haylton Rodrigues <haylton2012@gmail.com>
2018-07-31 21:36:40 -0300

    Adicionado métodos para as ações dos botões ac(allclear) e ce(clearEntry)

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,110 +1,182 @@
 //essa é a classe que contém as regras de negócio
-class CalcController{
+class CalcController {
     //método construtor é chamado automaticamente quando é feita uma nova instancia da classe
-    constructor(){
+    constructor() {
         //this faz referência ao próprio objeto que foi instanciado, fazendo referencia a atributos e métodos
-        this._displayCalcEl=  document.querySelector("#display");
+        this._operation = [];
+        this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
-        this._timeEl =  document.querySelector("#hora");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate; //a convensão pra saber se  o atributo é privado é utilizado o underline no começo
         this._locale = "pt-BR";
         this.initialize();
         this.initButtonEvents();
     }
 
-    initialize(){
+    initialize() {
         //se o código vai repetir, crie um método
         this.setDisplayDateTime();
         //essa função abaixo é do JS que atualiza o que esta dentro dela, e depois das chaves indica em quantos milesegundos
-      setInterval(()=>{
-        this.setDisplayDateTime()
-      },1000 );
+        setInterval(() => {
+            this.setDisplayDateTime()
+        }, 1000);
 
-      /*    
-      let interval = setInterval(()=>{
-        this.setDisplayDate = this.getCurrentDate.toLocaleDateString(this._locale);
-        this.setDisplayTime = this.getCurrentDate.toLocaleTimeString(this._locale);
-      },1000 );
-      
-      setTimeout(()=>{
-        clearInterval(interval);
-      }, 5000); //se eu quisses que parasse de atualizar a data e a hora*/
+        /*    
+        let interval = setInterval(()=>{
+          this.setDisplayDate = this.getCurrentDate.toLocaleDateString(this._locale);
+          this.setDisplayTime = this.getCurrentDate.toLocaleTimeString(this._locale);
+        },1000 );
+        
+        setTimeout(()=>{
+          clearInterval(interval);
+        }, 5000); //se eu quisses que parasse de atualizar a data e a hora*/
     }
 
     //método criado para tratar múltiplos eventos
-    addEventListenerAll(element, events, fn){
+    addEventListenerAll(element, events, fn) {
         //o split é para substituir o espaço entre as duas palavras e vai transformar em um array
         //como o split vai transformar em um array, consigo fazer um forEach
-       
-        events.split(' ').forEach(event =>{
+
+        events.split(' ').forEach(event => {
             //primeiro parâmetro dele é que evento que vocÊ quer, exemplo click, o segundo é o que eu devo fazer. O terceiro é caso ele click no botão e no texto ao mesmo tempo, não dispare o numero duas vezes
             element.addEventListener(event, fn, false); //pega esse elemento e adicione um evento para cada vez que rodar o forEach
         });
 
-        
+
     }
+
+    //método para limpar tudo
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError() {
+        this.setDisplayCalc = "Error";
+    }
+
+    //decidir qual a ação desse botão
+    execBtn(numero) {
+
+        switch (numero) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+
+                break;
+            case 'subtração':
+
+                break;
+            case 'divisao':
+
+                break;
+            case 'multiplicacao':
+
+                break;
+            case 'porcento':
+
+                break;
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(numero));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+
+        }
+
+    }
+
 
     //inicializar os buttons, o seletor de > g, quer dizer pegue todas as tags g do filho de buttons
 
-    initButtonEvents(){
-       let buttons = document.querySelectorAll("#buttons > g, #parts > g");
-       
-       buttons.forEach((btn, index)=>{
-       //primeiro parâmetro dele é que evento que vocÊ quer, exemplo click, o segundo é o que eu devo fazer. O e é o parametro da minha arrowFunction, isso é, se eu quiser falar algo sobre o button que foi chamado, eu falo algo    
-        this.addEventListenerAll(btn,'click drag', e=>{ //addEventListener  é nativo do JS
-            console.log(btn.className.baseVal.replace("btn-", ""));
-        });
+    initButtonEvents() {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
-            btn.style.cursor = "pointer";
-        });
+        buttons.forEach((btn, index) => {
+            //primeiro parâmetro dele é que evento que vocÊ quer, exemplo click, o segundo é o que eu devo fazer. O e é o parametro da minha arrowFunction, isso é, se eu quiser falar algo sobre o button que foi chamado, eu falo algo    
+            this.addEventListenerAll(btn, 'click drag', e => { //addEventListener  é nativo do JS
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
+            });
 
-       });
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+
+        });
 
     }
 
 
-    setDisplayDateTime(){
+    setDisplayDateTime() {
         this.setDisplayDate = this.getCurrentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
-            month:"short",
-            year:"numeric"
+            month: "short",
+            year: "numeric"
         });
         this.setDisplayTime = this.getCurrentDate.toLocaleTimeString(this._locale);
     }
 
-    get getDisplayTime(){
+    get getDisplayTime() {
         return this._timeEl.innerHTML;
     }
 
-    get getDisplayDate(){
+    get getDisplayDate() {
         return this._dateEl.innerHTML;
     }
 
-    set setDisplayTime(value){
-       this._timeEl.innerHTML = value;
+    set setDisplayTime(value) {
+        this._timeEl.innerHTML = value;
     }
 
-    set setDisplayDate(value){
+    set setDisplayDate(value) {
         this._dateEl.innerHTML = value;
     }
 
 
     //recupera o valor
-    get getDisplayCalc(){
+    get getDisplayCalc() {
         return this._displayCalcEl.innerHTML;
     }
 
     //altera o valor
-    set setDisplayCalc(valor){
+    set setDisplayCalc(valor) {
         this._displayCalcEl.innerHTML = valor;
     }
 
-    get getCurrentDate(){
+    get getCurrentDate() {
         return new Date();
     }
 
-    set setCurrentDate(value){
+    set setCurrentDate(value) {
         this._currentDate = value;
     }
 
@@ -119,7 +191,7 @@ funções(métodos) são ações/códigos que executam uma ação e retornam val
 /* métodos de acesso para atributos e métodos. Public é aberto pra todos, protected aberto apenas 
 para atributos e métodos da mesma classe e classe Pai
 Private somente atributos e métodos da própria classe
-*/ 
+*/
 /**
  * há somente duas cois a serem feitas com um atributo, ou eu recupero o valor que tá dentro dele
  * ou eu atribuo um valor para o atributo guardar 
